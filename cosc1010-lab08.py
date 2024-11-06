e7bf91b9cb846a02c98b8b1ce08d514a10ae4956
MMallberg <mmallber@uwyo.edu>
2024-11-06 00:00:50 +0000

    incomp

--- cosc1010-lab08.py
+++ cosc1010-lab08.py
@@ -1,8 +1,8 @@
-# Your Name Here
+# Marit Mallberg
 # UWYO COSC 1010
 # Submission Date
-# Lab XX
-# Lab Section:
+# Lab 07
+# Lab Section: 10
 # Sources, people worked with, help given to:
 # your
 # comments
@@ -14,6 +14,37 @@
 # Other wise return the converted int or float 
 # Floats should only have one decimal point in them 
 
+
+def numberswitch(string):
+    """switch a number into it's integer of float value"""
+    isneg = False
+    if string[0] == "-":
+        isneg = True
+        string = string.replace("-","")
+    if "." in string:
+        nums=string.split(".")
+        if len(nums)==2 and nums[0].isdigit() and nums[1].isdigit:
+            if isneg:
+                return -1*float(string)
+            else:
+                return float(string)
+    elif string.isdigit():
+        if isneg:
+            return -1*int(string)
+        else:
+            return int(string)
+    else:
+        return False
+        
+    
+
+my_number = "-170"
+my_float = "3.33"
+error_check = "cat"
+
+print(numberswitch(my_number))
+print(numberswitch(my_float))
+print(numberswitch(error_check))
 
 print("*" * 75)
 
@@ -38,6 +69,39 @@ print("*" * 75)
 # Remember all inputs are strings, but the function needs ints or floats
 # Call your function and print the resulting list
 
+def slope_intercept(m,b,l,u):
+    """gives all func values in range a to b"""
+    valuelist=[]
+    for numb in range(l,(u+1)):
+        value = m*numb + b
+        valuelist.append(value)
+    print(valuelist)
+
+
+while True:
+    inputs=input("give me a slope, y-intercept, and an upper and lower bound. enter 'exit' to exit")
+    if inputs.lower() == "exit":
+        break
+    inputs=inputs.replace(" ","")
+    numlist = inputs.split(",")
+    len_check=True
+    if len(numlist) != 4:
+        len_check=False
+    diglist=[]
+    for num in numlist:
+        num=numberswitch(num)
+        diglist.append(num)
+    upperfloatcheck = True
+    if type(diglist[2]) == float:
+        upperfloatcheck = False
+    lowerfloatcheck= True
+    if type(diglist[3]) == float:
+        lowerfloatcheck = False
+    boundcheck = True
+    if diglist[2]<diglist[3]:
+        boundcheck = False
+    slope_intercept(diglist[0],diglist[1],diglist[2],diglist[3])
+
 print("*" * 75)
 
 
@@ -48,3 +112,36 @@ print("*" * 75)
 # Create a loop like above to prompt the user for input for the three values
 # Create a second function that just does the square root operation 
     # If the number you are trying to take the square root of is negative, return null
+
+def sqrt(n):
+    """the sqrt function"""
+    
+    sn=n**(1/2)
+    return sn
+def quadformula(a,b,c):
+    """ solves the quadratic formula for x-intercepts"""
+    answers = []
+    xplus=(-b+sqrt(b**2-4*a*c))/2*a
+    answers.append(xplus)
+    xminus = (-b-sqrt(b**2-4*a*c))/2*a
+    answers.append(xminus)
+    print(answers)
+
+while True:
+    equation = input("give me the a, b, and c, type 'exit' to exit")
+    if equation.lower() == "exit":
+        break
+    equation = equation.replace(" ", "")
+    equlist = equation.split(",")
+    numlist = []
+    for equ in equlist:
+        equ = numberswitch(equ)
+        numlist.append(equ)
+    quadformula(numlist[0],numlist[1],numlist[2])
+
+
+
+
+
+
+
